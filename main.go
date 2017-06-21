bc40a533efeec10a3a64e5def760254fec1b5af8
cocaer <cocaer.cl@gmail.com>
2017-06-21 10:39:47 +0800

    .

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"time"
 
-	"github.com/cocaer/goNLP/seg/hmm"
+	"github.com/cocaer/goNLP/seg/bhmm"
 )
 
 func main() {
 	//		bhmm.HmmSaveTrainingFile()
-	k := time.Now()
-	m := hmm.NewModel()
-	in, _ := os.Open("/home/begosu/gocode/src/github.com/cocaer/goNLP/test/pku_test.utf8")
-	defer in.Close()
-	out, err := os.Create("./test/test.utf8")
-	if err != nil {
-		fmt.Print("create file error")
-	}
-	defer out.Close()
-	scanner := bufio.NewScanner(in)
-	for scanner.Scan() {
-		s := scanner.Text()
+	// k := time.Now()
+	// m := hmm.NewModel()
+	// in, _ := os.Open("/home/begosu/gocode/src/github.com/cocaer/goNLP/test/pku_test.utf8")
+	// defer in.Close()
+	// out, err := os.Create("./test/test.utf8")
+	// if err != nil {
+	// 	fmt.Print("create file error")
+	// }
+	// defer out.Close()
+	// scanner := bufio.NewScanner(in)
+	// for scanner.Scan() {
+	// 	s := scanner.Text()
 
-		words := m.Cut(s)
-		for _, word := range words {
-			out.WriteString(word + " ")
-		}
-		out.WriteString("\n")
-	}
-	fmt.Println(time.Now().Sub(k))
+	// 	words := m.Cut(s)
+	// 	for _, word := range words {
+	// 		out.WriteString(word + " ")
+	// 	}
+	// 	out.WriteString("\n")
+	// }
+	// fmt.Println(time.Now().Sub(k))
+	m := bhmm.NewModel() //基于hmm的优化实现
+	//m := hmm.NewModel() //纯hmm实现，没有优化
+	fmt.Println(m.Cut("结婚的和尚未结婚的"))
 }
--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ import "github.com/cocaer/goNLP/seg/bhmm"
 import "github.com/cocaer/goNLP/seg/mm"
 import "fmt"
 func main() {
-    //m := mm.NewModel()
+    //m := mm.NewModel() //提供最大正向和逆向匹配
     m :=bhmm.NewModel() //基于hmm的优化实现
     //m := hmm.NewModel() //纯hmm实现，没有优化
     fmt.Println(m.Cut("结婚的和尚未结婚的"))
@@ -21,7 +21,7 @@ func main() {
 ```
 
 ``` txt
-output:[]
+output:[结婚 的 和 尚未 结婚 的]
 ```
 
 
